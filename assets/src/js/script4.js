a69e58bc811ebb8a98f0b8faad23f8dd5328c532
Fleur geoffrey <fleur.geoffey@orange.fr>
2022-11-02 11:39:12 +0100

    Ajout exercice 4

--- assets/src/js/script4.js
+++ assets/src/js/script4.js
@@ -1,17 +1,30 @@
 let pu = parseInt(prompt("Saisie du prix unitaire"));
 let qTecom = parseInt(prompt("Quantité souhaitez"));
 let tot = pu * qTecom;
-let port = 1.02;
+let port = 0;
+let reduc = 0.9;
+let totReduc = tot * reduc;
 
 console.log(tot);
 
 if (tot >= 100 && tot <= 200) {
-    totReduc = tot * 0.95;
+    port = 6;
+    reduc = 0.95;
+    totReduc = tot * reduc;
+    totFinal = totReduc + port;
+    document.write("Le total à payer est de : " + totFinal + "€");
+    } else if (tot > 200 && totReduc <= 500) {
+    port = 1.02;
+    reduc = 0.9;
+    totReduc = tot * reduc;
     totFinal = totReduc * port;
     document.write("Le total à payer est de : " + totFinal + "€");
-} else if (tot > 200) {
-    totReduc = tot * (0.9 + port);
-    totFinal = totReduc * port;
+    } else if (tot < 100) {
+    port = 6;
+    totFinal = tot + port;
     document.write("Le total à payer est de : " + totFinal + "€");
+    } else if (totReduc > 500) {
+    document.write("Le total à payer est de : " + totReduc + "€");
 }
+
 
